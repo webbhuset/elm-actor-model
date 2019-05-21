13c9efb26439cf4f7ea0e20600c9a132584c53a2
Albert Dahlin <info@albertdahlin.com>
2019-05-21 07:34:26 +0200

    Exposed SystemEvent

--- src/Webbhuset/Component/SystemEvent.elm
+++ src/Webbhuset/Component/SystemEvent.elm
@@ -1,7 +1,31 @@
 module Webbhuset.Component.SystemEvent exposing (SystemEvent(..))
 
+{-|
+
+# System Events
+
+- **Gone**: If you send a message to a PID that does not exist
+  anymore you will receive a `Gone` event.
+- **Kill** is received when your component is going to be killed.
+
+@docs SystemEvent
+
+-}
 import Webbhuset.Internal.PID exposing (PID)
 
+
+{-| System Event
+
+    onSystem : SystemEvent -> Maybe MsgIn
+    onSystem event =
+        case event of
+            Gone pid ->
+                UnObserveData pid
+                    |> Just
+
+            Kill ->
+                Nothing
+-}
 type SystemEvent
     = Gone PID
     | Kill
