aad77ff7680f6392000b0407f3bcffb95be5c2b8
Albert Dahlin <info@albertdahlin.com>
2019-05-31 22:46:44 +0200

    Added some sandbox assert examples

---
+++ example/assert/AssertExample1.elm
@@ -0,0 +1,128 @@
+module AssertExample1 exposing
+    ( MsgIn(..)
+    , MsgOut(..)
+    , Model
+    , component
+    )
+
+import Html exposing (Html)
+import Html.Attributes as HA
+import Html.Events as Events
+import Webbhuset.Component as Component exposing (PID)
+import Webbhuset.Component.SystemEvent as SystemEvent exposing (SystemEvent)
+
+
+
+
+type MsgIn
+    = GoodButtonClicked
+    | BadButtonClicked
+
+
+type MsgOut
+    = SomethingGood Int
+    | SomethingBad
+
+
+type alias Model =
+    { pid : PID
+    , howGood : Int
+    }
+
+
+--
+-- Component
+--
+
+component : Component.UI Model MsgIn MsgOut
+component =
+    { init = init
+    , update = update
+    , view = view
+    , onSystem = onSystem
+    , subs = subs
+    }
+
+
+init : PID -> ( Model , List MsgOut, Cmd MsgIn )
+init pid =
+    ( { pid = pid
+      , howGood = 0
+      }
+    , []
+    , Cmd.none
+    )
+
+
+onSystem : SystemEvent -> SystemEvent.Handling MsgIn
+onSystem event =
+    SystemEvent.default
+
+
+subs : Model -> Sub MsgIn
+subs model =
+    Sub.none
+
+
+update : MsgIn -> Model -> ( Model, List MsgOut, Cmd MsgIn )
+update msgIn model =
+    case msgIn of
+        GoodButtonClicked ->
+            ( { model | howGood = model.howGood + 1 }
+            , [ SomethingGood (model.howGood + 1)
+              ]
+            , Cmd.none
+            )
+
+        BadButtonClicked ->
+            ( { model | howGood = model.howGood - 1 }
+            , [ SomethingBad
+              ]
+            , Cmd.none
+            )
+
+view : Model -> Html MsgIn
+view model =
+    Html.div
+        [ HA.class "example"
+        ]
+        [ Html.node "style" [] [ Html.text css ]
+        , Html.button
+            [ Events.onClick GoodButtonClicked
+            ]
+            [ Html.text "Good button"
+            ]
+        , Html.button
+            [ Events.onClick BadButtonClicked
+            ]
+            [ Html.text "Bad button"
+            ]
+        , Html.span
+            []
+            [ Html.text "My Status: "
+            , Html.text
+                (if model.howGood < 0 then
+                    "This sucks!!!"
+                else if model.howGood == 0 then
+                    "Bad"
+                else if model.howGood == 1 then
+                    "Not that bad"
+                else if model.howGood == 2 then
+                    "Acceptable (lagom)"
+                else if model.howGood == 3 then
+                    "Good"
+                else if model.howGood == 4 then
+                    "Really Good"
+                else
+                    "Can't get better than this"
+                )
+            ]
+        ]
+
+
+css =
+    """
+.example > * {
+    margin: 0.5rem;
+}
+"""
---
+++ example/assert/AssertExample1Sandbox.elm
@@ -0,0 +1,179 @@
+module Html.UIComponentSandbox exposing (..)
+
+import Webbhuset.Component.Sandbox as Sandbox exposing (SandboxProgram)
+import Webbhuset.PID as PID
+import AssertExample1 as Example
+import Html exposing (Html)
+import Html.Attributes as HA
+
+
+main : SandboxProgram Example.Model Example.MsgIn Example.MsgOut
+main =
+    Sandbox.ui
+        { title = "Examples of Asserts"
+        , component = Example.component
+        , cases =
+            [ test_click
+            , test_timeout
+            , test_timeout2
+            , test_timeout3
+            ]
+        , stringifyMsgIn = Debug.toString
+        , stringifyMsgOut = Debug.toString
+        , wrapView = view
+        }
+
+view : Html Example.MsgIn -> Html Example.MsgIn
+view componentHtml =
+    Html.div
+        [ HA.class "component"
+        ]
+        [ Html.node "style" [] [ Html.text css ]
+        , componentHtml
+        ]
+
+
+css : String
+css =
+    """
+.component {
+    font-family: monospace;
+    padding: 1rem;
+}
+"""
+
+
+test_click : Sandbox.TestCase Example.MsgIn Example.MsgOut
+test_click =
+    { title = "Assert out messages"
+    , desc =
+    """
+Click the "Good Button" until the level becomes "Really good" for this test to pass.
+
+Levels are:
+- This sucks!!!
+- Bad
+- Not that bad
+- Acceptable
+- Good
+- Really Good
+- Too good
+
+    """
+    , init =
+        [
+        ]
+    , onMsgOut = \outMsg ->
+        case outMsg of
+            Example.SomethingBad ->
+                [ Sandbox.fail "You ruined it...."
+                ]
+
+            Example.SomethingGood 4 ->
+                [ Sandbox.pass
+                ]
+
+            Example.SomethingGood 5 ->
+                [ Sandbox.fail "Now it was too good, don't get carried away."
+                ]
+
+            _ ->
+                [ ]
+    }
+
+test_timeout : Sandbox.TestCase Example.MsgIn Example.MsgOut
+test_timeout =
+    { title = "Timeout"
+    , desc =
+    """
+Once you clicked "Good Button" you have one second to reach "Acceptable".
+Try waiting and the test will fail.
+    """
+    , init =
+        [
+        ]
+    , onMsgOut = \outMsg ->
+        case outMsg of
+            Example.SomethingBad ->
+                [ Sandbox.fail "You ruined it...."
+                ]
+
+            Example.SomethingGood 1 ->
+                [ Sandbox.timeout 1000
+                ]
+
+            Example.SomethingGood 2 ->
+                [ Sandbox.pass
+                ]
+
+            _ ->
+                [ Sandbox.fail "Don't get carried away."
+                ]
+    }
+
+
+test_timeout2 : Sandbox.TestCase Example.MsgIn Example.MsgOut
+test_timeout2 =
+    { title = "Timeout on next update"
+    , desc =
+    """
+If you set the timeout to zero the test will timeout on next animation frame.
+This has to do with how Elm batches updates and synchronizes them with
+`requestAniamtionFrame`.
+
+Since `GoodButtonClicked` is sent directly from init this test passes.
+    """
+    , init =
+        [ Sandbox.timeout 0
+        , Sandbox.sendMsg Example.GoodButtonClicked
+        , Sandbox.sendMsg Example.GoodButtonClicked
+        ]
+    , onMsgOut = \outMsg ->
+        case outMsg of
+            Example.SomethingBad ->
+                [ Sandbox.fail "You ruined it...."
+                ]
+
+            Example.SomethingGood 2 ->
+                [ Sandbox.pass
+                ]
+
+            _ ->
+                [ ]
+    }
+
+
+test_timeout3 : Sandbox.TestCase Example.MsgIn Example.MsgOut
+test_timeout3 =
+    { title = "Timeout is unpredictable"
+    , desc =
+    """
+Here, the timeout is set to 30ms. Two messages are sent in a sequence with
+10ms delay for each. Sometimes this test passes and sometimes it fails, try
+reloading the test multiple times. This is
+due to Elm's internals (I think) trying to syncronize updates with request
+animation frame.
+    """
+    , init =
+        [ Sandbox.timeout 30
+        , Sandbox.sendMsg Example.GoodButtonClicked
+            |> Sandbox.delay 10
+        ]
+    , onMsgOut = \outMsg ->
+        case outMsg of
+            Example.SomethingBad ->
+                [ Sandbox.fail "You ruined it...."
+                ]
+
+            Example.SomethingGood 1 ->
+                [ Sandbox.sendMsg Example.GoodButtonClicked
+                    |> Sandbox.delay 10
+                ]
+
+            Example.SomethingGood 2 ->
+                [ Sandbox.pass
+                ]
+
+            _ ->
+                [ ]
+    }
